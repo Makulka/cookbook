c8093be3f44ab57c11cb2f8f2d4ee8a6da8686ba
Stania Svobodova <greg.morris85@gmail.com>
2019-01-06 15:25:34 +0000

    app prepared for production for websocket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://cookbook-ss.herokuapp.com/cable" 
   config.action_cable.allowed_request_origins = ['https://cookbook-ss.herokuapp.com/']
 end
