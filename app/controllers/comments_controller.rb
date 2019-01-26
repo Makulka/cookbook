db075b30881b438a63c5d656b2471eef0ebe77c5
Stania Svobodova <greg.morris85@gmail.com>
2019-01-26 10:48:20 +0000

    addition of comments fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,17 @@
 class CommentsController < ApplicationController
     
     before_action :require_user
+    before_action :find_recipe
     
     def create
-        @recipe = Recipe.find(params[:recipe_id])
-        @comment = @recipe.comments.build(comment_params)
+        #@comment = @recipe.comments.build(comment_params) - this cannot be used with validations in the model
+        @comment = Comment.new(comment_params)
+        @comment.recipe_id = @recipe.id
         @comment.user_id = current_user.id
         if @comment.save
             flash.now[:success] = "Thank you for your comment."
         else 
-            flash.now[:danger] = "Please enter a comment before submitting it."
+            flash.now[:danger] = "Please enter a comment containing 1-300 characters."
         end
         #redirect_to @recipe
         respond_to do |format|
@@ -18,7 +20,7 @@ class CommentsController < ApplicationController
     end
     
     def destroy
-        @recipe = Recipe.find(params[:recipe_id])
+        
         @comment = Comment.find(params[:id])
         @comment.destroy
         flash.now[:success] = "Your comment was deleted."
@@ -31,5 +33,9 @@ class CommentsController < ApplicationController
     private
     def comment_params
         params.require(:comment).permit(:body, :rating) 
+    end
+    
+    def find_recipe
+        @recipe = Recipe.find(params[:recipe_id])
     end
 end
